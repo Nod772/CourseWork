e4259145c9c914f46ae26db63f9693de04526da6
Nod772 <podnizhni@ukr.net>
2020-05-04 18:51:41 +0300

    DTO

---
+++ BaseTestLib/TestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseTestLib
+{
+    class TestModel:DbContext
+    {
+        public TestModel():base("name=TestModel")
+        {
+
+        }
+        public virtual DbSet<AnswerOption> AnswerOptions { get; set; }
+        public virtual DbSet<Question> Questions { get; set; }
+        public virtual DbSet<Test> Tests { get; set; }
+        public virtual DbSet<Teasher>  Teashers { get; set; }
+
+    }
+}
