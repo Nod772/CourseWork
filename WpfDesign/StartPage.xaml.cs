7485ae3d39f2649aeab3ab44808a2dcf8cc45e0b
VerusVladslav <vladislav.verus@gmail.com>
2020-05-07 21:29:53 +0300

    LoginForm

---
+++ WpfDesign/StartPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDesign
+{
+    /// <summary>
+    /// Interaction logic for StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Window
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ WpfDesign/TeacherForm.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDesign
+{
+    /// <summary>
+    /// Interaction logic for TeacherForm.xaml
+    /// </summary>
+    public partial class TeacherForm : Window
+    {
+        public TeacherForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
